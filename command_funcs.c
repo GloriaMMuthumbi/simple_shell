adceb6087aca9c4172fd0d71beeea9a6e7207b28
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-11-16 03:37:51 -0800

    task 2

--- command_funcs.c
+++ command_funcs.c
@@ -26,6 +26,7 @@ char *read_command(void)
 			return (NULL);
 		}
 	}
+
 	if (characters > 0 && line[characters - 1] == '\n')
 		line[characters - 1] = '\0';
 
@@ -40,16 +41,23 @@ char *read_command(void)
 void execute_command(char *command)
 {
 	pid_t pid;
-	int status;
+	int status, i = 0;
+	char *token;
 
-	char **args = malloc(2 * sizeof(char *));
+	char **args = malloc((MAX_ARGS + 1) * sizeof(char *));
 	if (args == NULL)
 	{
 		perror("memory allocation error");
 		return;
 	}
-	args[0] = command;
-	args[1] = NULL;
+	token = str_tok(command, " ");
+	while (token != NULL && i < MAX_ARGS)
+	{
+		args[i] = str_dup(token);
+		token = str_tok(NULL, " ");
+		i++;
+	}
+	args[i] = NULL;
 
 	pid = fork();
 	if (pid == -1)
@@ -60,6 +68,7 @@ void execute_command(char *command)
 	}
 	if(pid == 0)
 	{
+		fflush(stdout);
 		if (execve(command, args, environ) == -1)
 		{
 			perror("execve failed");
--- string_funcs.c
+++ string_funcs.c
@@ -7,7 +7,96 @@
 int str_len(char *s)
 {
 	int len = 0;
+
 	while (s[len] != '\0')
 		len++;
 	return (len);
+}
+
+/**
+ * str_tok - tokenizes a string
+ * @str: string to be tokenized
+ * @tok: token to be used as delimeter
+ * Return: pointer to next token
+ */
+char *str_tok(char *str, const char *tok)
+{
+	char *startToken;
+	static char *lastToken;
+
+	if (str)
+		startToken = str;
+	else if (lastToken)
+		startToken = lastToken;
+	else
+		return (NULL);
+
+	startToken += delim_check(*startToken, tok);
+
+	if (*startToken == '\0')
+	{
+		lastToken = NULL;
+		return (NULL);
+	}
+	lastToken = startToken + 1;
+	while (*lastToken && !delim_check(*lastToken, tok))
+		lastToken++;
+
+	if (*lastToken == '\0')
+		lastToken = NULL;
+	else
+	{
+		*lastToken = '\0';
+		lastToken++;
+	}
+	return (startToken);
+}
+
+/**
+ * delim_check - checks for delimeters
+ * @c: character to be checked
+ * @str: string to check
+ * Return: return 0 always
+ */
+unsigned int delim_check(char c, const char *str)
+{
+	while (*str)
+	{
+		if (c == *str)
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+/**
+ * str_dup - duplicates a string
+ * @str: string to be duplicated
+ * Return: returns pointer to duplicate
+ */
+char *str_dup(char *str)
+{
+	int len = str_len(str);
+	char *dup = (char *)malloc((len + 1) * sizeof(char));
+
+	if (dup == NULL)
+		return (NULL);
+
+	str_cpy(dup, str);
+	return (dup);
+}
+
+/**
+ * str_cpy - copies a string
+ * @dest: the string to be copied to
+ * @src: the string to be copied from
+ * Return: returns pointer to the dest string
+ */
+char *str_cpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while ((dest[i] = src[i]) != '\0')
+		i++;
+	return (dest);
 }
